f298ca9ea817144aea800beb6f3990619ba65a93
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-02 22:48:24 +0530

    added node modules

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //jshint esversion:6
+const express = require("express")
+const ejs = require("ejs")
+const bodyParser = require("body-parser")
+const { appendFileSync } = require("fs")
+
+const app = express()
+
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+//////listening//////
+
+app.listen(3000, function () {
+  console.log("server started successfully on port 3000")
+})
