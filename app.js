9ed987be3d4ee250fca38b1794e200a9e4511329
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-03 23:34:13 +0530

    used md5 to hash password to increase security

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const { appendFileSync } = require("fs")
 const mongoose = require("mongoose")
+const md5 = require("md5")
 
 const app = express()
 
@@ -40,7 +41,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   })
 
   newUser.save(function (err) {
@@ -54,7 +55,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username
-  const password = req.body.password
+  const password = md5(req.body.password)
 
   User.findOne({ email: username }, function (err, founduser) {
     if (err) {
