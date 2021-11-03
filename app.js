f92082705c180d6714514108d60e9a907c8b1c62
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-04 00:19:04 +0530

    removed md5 and used bcrypt to increase security

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const { appendFileSync } = require("fs")
 const mongoose = require("mongoose")
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = express()
 
@@ -39,34 +40,38 @@ app.get("/register", function (req, res) {
 })
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  })
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    })
 
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render("secrets")
-    }
+    newUser.save(function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        res.render("secrets")
+      }
+    })
   })
 })
 
 app.post("/login", function (req, res) {
   const username = req.body.username
-  const password = md5(req.body.password)
+  const password = req.body.password
 
   User.findOne({ email: username }, function (err, founduser) {
     if (err) {
       console.log(err)
     }
     if (founduser) {
-      if (founduser.password === password) {
-        res.render("secrets")
-      } else {
-        console.log("incorrect password")
-      }
+      bcrypt.compare(password, founduser.password, function (err, result) {
+        if (result === true) {
+          res.render("secrets")
+        } else {
+          console.log("Incorrect password")
+        }
+      })
     }
   })
 })
