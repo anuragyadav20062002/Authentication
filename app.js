352073abf32842786c49b3586031c356c330c586
anuragyadav20062002 <anuragyadav20062002@gmail.com>
2021-11-04 23:06:51 +0530

    added google auth function

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose")
 const session = require("express-session")
 const passport = require("passport")
 const passportLocalMongoose = require("passport-local-mongoose")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
 
 const app = express()
 
@@ -45,6 +46,22 @@ passport.use(User.createStrategy())
 
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CID,
+      clientSecret: process.env.CSECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/outh2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user)
+      })
+    }
+  )
+)
 
 app.get("/", function (req, res) {
   res.render("home")
